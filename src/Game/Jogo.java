e9555e9682c6d1798107b104fd89393d40f396e5
Matheus <Matheus@MATHEUS-LIMA>
2019-07-03 15:05:52 -0300

    Frame e canvas

--- src/Game/Jogo.java
+++ src/Game/Jogo.java
@@ -1,9 +1,34 @@
 package Game;
 
-public class Jogo {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Jogo extends Canvas implements Runnable {
+
+	public static JFrame frame;
+	private final int WIDITH = 160, HEIGHT = 120, SCALE = 3;
+
+	public Jogo() {
+		setPreferredSize(new Dimension(WIDITH * SCALE, HEIGHT * SCALE));
+		frame = new JFrame("Projeto Jogo");
+		frame.add(this);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Jogo jogo = new Jogo();
+	}
+
+	@Override
+	public void run() {
 
 	}
 
