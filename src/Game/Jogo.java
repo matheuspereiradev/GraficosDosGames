13b317d0b59526762544ce35dbf2ef2b2a96ac9d
Matheus <Matheus@MATHEUS-LIMA>
2019-07-03 17:12:28 -0300

    Renderizando e iniciando com buffers

--- src/Game/Jogo.java
+++ src/Game/Jogo.java
@@ -1,8 +1,13 @@
 package Game;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
+import javax.swing.GrayFilter;
 import javax.swing.JFrame;
 
 public class Jogo extends Canvas implements Runnable {
@@ -10,11 +15,13 @@ public class Jogo extends Canvas implements Runnable {
 	private Thread thread;
 	private boolean isRunning;
 	public static JFrame frame;
-	private final int WIDITH = 160, HEIGHT = 120, SCALE = 3;
+	private final int WIDITH = 160, HEIGHT = 120, SCALE = 4;
+	private BufferedImage background;
 
 	public Jogo() {
 		setPreferredSize(new Dimension(WIDITH * SCALE, HEIGHT * SCALE));
 		iniciarFrame();
+		background=new BufferedImage(WIDITH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 	}
 
 	public static void main(String[] args) {
@@ -33,39 +40,51 @@ public class Jogo extends Canvas implements Runnable {
 	}
 
 	public void atualizar() {
-		//System.out.println("Tick");
+		// System.out.println("Tick");
 	}
 
 	public void renderizar() {
-		//System.out.println("Render");
+		BufferStrategy bs= this.getBufferStrategy();
+		if(bs==null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g=background.getGraphics();
+		g.setColor(new Color(255,40,40));
+		g.fillRect(0, 0, WIDITH ,HEIGHT);
+		g=bs.getDrawGraphics();
+		g.drawImage(background, 0, 0, WIDITH*SCALE, HEIGHT*SCALE, null);
+		bs.show();
 	}
 
 	@Override
 	public void run() {
-		long lastTime = System.nanoTime();//ultima vez que foi executada a atualiação
-		double amountOfTicks = 60.0;//quantidade de atualizações por segundo
-		double ns = 1000000000 / amountOfTicks;//"constante" do momento certo do update do jogo para ficar na quantidade de fps descritas no amountOfTicks
+		long lastTime = System.nanoTime();// ultima vez que foi executada a atualiação
+		double amountOfTicks = 60.0;// quantidade de atualizações por segundo
+		double ns = 1000000000 / amountOfTicks;// "constante" do momento certo do update do jogo para ficar na
+												// quantidade de fps descritas no amountOfTicks
 		double delta = 0;
 		int frames = 0;
-		double timer=System.currentTimeMillis();
+		double timer = System.currentTimeMillis();
 		while (isRunning) {
-			long now = System.nanoTime();//tempo atual
+			long now = System.nanoTime();// tempo atual
 			delta += (now - lastTime) / ns;
-			lastTime=now;//substitui a ultima execução do while pelo tempo atual
-			
+			lastTime = now;// substitui a ultima execução do while pelo tempo atual
+
 			if (delta >= 1) {
 				atualizar();
 				renderizar();
 				frames++;
 				delta--;
 			}
-			
-			if(System.currentTimeMillis()-timer>=1000) {
-				System.out.println("FPS="+frames);
-				frames=0;
-				timer=System.currentTimeMillis();//atualiza o tempo para o tempo atual
-				//ou timer+=1000; para dizer que se passaram 1000 milesegundos desde o valor inicial do timer
-				
+
+			if (System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS=" + frames);
+				frames = 0;
+				timer = System.currentTimeMillis();// atualiza o tempo para o tempo atual
+				// ou timer+=1000; para dizer que se passaram 1000 milesegundos desde o valor
+				// inicial do timer
+
 			}
 
 		}
