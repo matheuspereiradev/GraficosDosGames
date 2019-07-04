f8ffaf2513b238a287d6025948f7a1c075a41fe9
Matheus <Matheus@MATHEUS-LIMA>
2019-07-04 01:45:56 -0300

    Animações e rotacionando objetos

--- src/Game/Jogo.java
+++ src/Game/Jogo.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
@@ -16,18 +17,24 @@ public class Jogo extends Canvas implements Runnable {
 	public static JFrame frame;
 	private final int WIDITH = 240, HEIGHT = 160, SCALE = 2;
 	private BufferedImage background;
-	
+
 	private Spritesheet sheet;
-	private BufferedImage jogador;
-	
-	private int x=0;
+	private BufferedImage[] jogador;
+	private BufferedImage monstrinho;
+	private int framesJogador = 0, maxframesJogador = 20, curAnimation = 0, maxAnimationJogador=3;
+
+	private int x = 0, y = 0;
 
 	public Jogo() {
-		sheet=new Spritesheet("/Spritesheet.png");
-		jogador=sheet.getSprite(0, 0, 16, 16);
-		setPreferredSize(new Dimension(WIDITH * SCALE, HEIGHT * SCALE));//tamanho da janela
+		sheet = new Spritesheet("/Spritesheet.png");
+		jogador = new BufferedImage[3];
+		jogador[0] = sheet.getSprite(0, 0, 16, 16);
+		jogador[1] = sheet.getSprite(16, 0, 16, 16);
+		jogador[2] = sheet.getSprite(32, 0, 16, 16);
+		monstrinho = sheet.getSprite(0, 16, 16, 16);
+		setPreferredSize(new Dimension(WIDITH * SCALE, HEIGHT * SCALE));// tamanho da janela
 		iniciarFrame();
-		background=new BufferedImage(WIDITH, HEIGHT, BufferedImage.TYPE_INT_RGB);//imagem do fundo
+		background = new BufferedImage(WIDITH, HEIGHT, BufferedImage.TYPE_INT_RGB);// imagem do fundo
 	}
 
 	public static void main(String[] args) {
@@ -46,35 +53,61 @@ public class Jogo extends Canvas implements Runnable {
 	}
 
 	public void atualizar() {
-		x++;
+		if (x >= WIDITH) {
+			x = 0;
+		} else {
+			x++;
+		}
+		if (y >= HEIGHT) {
+			y = 0;
+		} else {
+			y++;
+		}
+
+		framesJogador++;
+		if (framesJogador >= maxframesJogador) {
+			framesJogador = 0;
+			curAnimation++;
+			if (curAnimation >= maxAnimationJogador) {
+				curAnimation = 0;
+			}
+		}
 	}
 
 	public void renderizar() {
-		BufferStrategy bs= this.getBufferStrategy();
-		if(bs==null) {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
 			this.createBufferStrategy(3);
 			return;
 		}
-		Graphics g=background.getGraphics();
-		g.setColor(new Color(255,40,40));
-		g.fillRect(0, 0, WIDITH ,HEIGHT);
-		
+		Graphics g = background.getGraphics();
+		g.setColor(new Color(63, 68, 152));
+		g.fillRect(0, 0, WIDITH, HEIGHT);
+
 		g.setColor(Color.CYAN);
-		g.fillRect(20, 20,80, 80);
-		
+		g.fillRect(20, 20, 80, 80);
+
 		g.setColor(Color.GREEN);
 		g.fillOval(35, 25, 40, 40);
-		
-		g.setFont(new Font("Arial", Font.BOLD,20 ));
+
+		g.setFont(new Font("Arial", Font.BOLD, 20));
 		g.setColor(Color.BLACK);
 		g.drawString("Hello word", 40, 50);
-		
-		g.drawImage(jogador, x, 25, null);
-		
-		
-		g.dispose();//limparr dados da imagem que nao foram usados
-		g=bs.getDrawGraphics();
-		g.drawImage(background, 0, 0, WIDITH*SCALE, HEIGHT*SCALE, null);
+
+		/* renderização do jogo */
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(new Color(0, 0, 0, 0));
+		g2.fillRect(0, 0, WIDITH, HEIGHT);// deixar escuro
+		g2.rotate(Math.toRadians(180), x + 8, 25 + 8);
+		g.drawImage(monstrinho, x, 25, null);
+		g2.rotate(Math.toRadians(-180), x + 8, 25 + 8);
+		g.drawImage(jogador[curAnimation], 65, y, null);
+
+		/**/
+
+		g.dispose();// limpar dados da imagem que nao foram usados
+		g = bs.getDrawGraphics();
+		g.drawImage(background, 0, 0, WIDITH * SCALE, HEIGHT * SCALE, null);
 		bs.show();
 	}
 
@@ -115,13 +148,13 @@ public class Jogo extends Canvas implements Runnable {
 
 	public synchronized void iniciar() {
 		thread = new Thread(this);
-		thread.start();//chama o run da thread
+		thread.start();// chama o run da thread
 		isRunning = true;
 
 	}
 
 	public synchronized void parar() {
-		isRunning =false;
+		isRunning = false;
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
